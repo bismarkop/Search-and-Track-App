7b32644eed8c693403e60f5e34ccac382aef24c7
biso <bopoku89@gmail.com>
2024-06-24 11:30:34 -0400

    Added json files and updated form

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
 
         const company = document.getElementById('company').value;
+        const position = document.getElementById('position').value;
         const date = document.getElementById('date').value;
         const status = document.getElementById('status').value;
         const stages = document.getElementById('stages').value;
         const notes = document.getElementById('notes').value;
 
-        addJob(company, date, status, stages, notes);
+        addJob(company, position, date, status, stages, notes);
 
         jobForm.reset();
     });
 
-    function addJob(company, date, status, stages, notes) {
+    function addJob(company, position, date, status, stages, notes) {
         const row = document.createElement('tr');
+        
 
         row.innerHTML = `
             <td>${company}</td>
+            <td>${position}</td>
             <td>${date}</td>
             <td>${status}</td>
             <td>${stages}</td>
@@ -44,3 +47,4 @@ document.addEventListener('DOMContentLoaded', () => {
     //     }
     // }
 });
+
