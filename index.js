cee3a31f591ca41632e6e2f44b188424cb26c810
biso <bopoku89@gmail.com>
2024-06-26 01:35:06 -0400

    Cleaned up js file

--- index.js
+++ index.js
@@ -35,48 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
 
         jobList.appendChild(row);
-    }
-
-
-    
+    }  
 });
 
-const jobUrl = "https://jobicy.com/api/v2/remote-jobs"
-
-async function receiveInput() {
-    let jobListings = []
-
-    fetch(jobUrl)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        jobListings = data.jobs
-        console.log(jobListings)
-
-        for (let i = 0; i < jobListings.length; i++) {
-            const jobPost = jobListings[i];
-            let jobDiv = document.createElement('div')
-
-            jobDiv.value = jobPost.id
-            jobDiv.innerHTML = `${jobPost.companyName}`
-            searchResults.appendChild(jobDiv)
-            
-        }
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
-
-    const searchInput = searchBar.value
-    // searchResults.innerHTML = ""
-    console.log(searchInput)
-
-    if (searchInput === 0) return
-
-    const results = await fetchCompanyInfo(searchInput)
-}
-// receiveInput()
 
 async function fetchCompanyInfo(info) {
     try {
@@ -85,23 +46,16 @@ async function fetchCompanyInfo(info) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        // filter?? COME BACK TO THIS
         const jobs = data.jobs
-        // jobs = jobs.filter(data => data.areas.includes(info))
-
-
         
         for (let i = 0; i < jobs.length; i++) {
             const jobPost = jobs[i];
             let jobDiv = document.createElement('div')
-            // let jobPostUrl = document.
-
             jobDiv.value = jobPost.id
+
             jobDiv.innerHTML = `<br>${jobPost.companyName}: ${jobPost.jobDescription} To apply, click the link - <a href=${jobPost.url}>${jobPost.url}</a>` 
             searchResults.appendChild(jobDiv)
-            console.log(jobPost.jobDescription)
         }
-        console.log(jobs)
         return data.jobs;
     } catch (error) {
         console.error('Fetch error:', error);
